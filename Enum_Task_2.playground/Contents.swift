439fa226dd1c2253208f58dd5c8a6a32884ac0d9
fuadrustamov999 <56446556+fuadrustamov999@users.noreply.github.com>
2020-03-14 22:47:16 +0400

    Initial commit

---
+++ Enum_Task_2.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+enum Color {        //Contains two possible colors of chessman
+    case white
+    case black
+}
+enum Chessmen {
+    case rook(nameInRus: String,color: Color)
+    case knight(nameInRus: String,color: Color)
+    case bishop(nameInRus: String,color: Color)
+    case queen(nameInRus: String,color: Color)
+    case king(nameInRus: String,color: Color)
+    case pawn(nameInRus: String,color: Color)
+}
+
+let chessman : Chessmen
+chessman = .pawn(nameInRus:"Пешка",color: .black)
+
+let chessman2 = Chessmen.knight(nameInRus: "Конь", color: .white)
+
+let chessman3 = Chessmen.bishop(nameInRus: "Слон", color: .black)
