66979f50916eed70e85844135e85b91b32b6ae00
fuadrustamov999 <56446556+fuadrustamov999@users.noreply.github.com>
2020-03-14 21:48:42 +0400

    Initial commit

---
+++ Enum_Task_1.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+enum Chessmen {
+    case rook
+    case knight
+    case bishop
+    case queen
+    case king
+    case pawn
+}
+
+var chessman = Chessmen.knight
+var chessman2 : Chessmen
+chessman2 = .king
+var chessman3 = Chessmen.rook
+var chessman4 : Chessmen
+chessman4 = .queen
+var chessman5 = Chessmen.bishop
+var chessman6 : Chessmen
+chessman6 = .pawn
